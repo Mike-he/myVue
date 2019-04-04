30626810fb8c3a40d7b61c3cf54f764802e74038
hequan <hequan@xjk.com>
2019-04-04 13:46:10 +0800

    update login

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      component: () => import('../components/login/Login')
+      component: () => import('../components/Login')
     },
     {
       path: '/order',
