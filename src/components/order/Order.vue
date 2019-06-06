b656d161d4f391a7d8b1de54c6b5b6a27f96521b
MikeHe <mike_hequan@163.com>
2019-06-06 14:33:22 +0800

    fix display bug

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -337,11 +337,11 @@
   }
 
   .animate {
+    display: flex;
     margin-left: 330px;
   }
 
   .logo-title {
-    position: absolute;
     color: #fff;
     font-size: 1.6rem;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
